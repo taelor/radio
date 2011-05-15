116f61bce7262abffd290e8912163ae83d76431d
Taylor Redden <taelor@co-hack.com>
2011-05-15 15:34:17 -0500

    adding reset_password_sent_at to users table for recoverable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110423204222) do
+ActiveRecord::Schema.define(:version => 20110515203033) do
 
   create_table "conferences", :force => true do |t|
     t.date     "start_date"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(:version => 20110423204222) do
     t.string   "cell_phone"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "password_salt"
     t.string   "authentication_token"
     t.string   "confirmation_token"
@@ -78,20 +78,16 @@ ActiveRecord::Schema.define(:version => 20110423204222) do
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                     :default => 0
+    t.integer  "failed_attempts",                       :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.integer  "role_id"
+    t.datetime "reset_password_sent_at"
   end
-
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
 end
