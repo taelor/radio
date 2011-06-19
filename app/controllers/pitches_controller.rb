c4a21058c0a1666689ded435aaceedc6e9df1073
Taylor Redden <taelor@co-hack.com>
2011-06-19 15:40:25 -0500

    working on some of the non logged in functionality. giving anon the abiltity to pitch shows to us.

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,2 +1,24 @@
 class PitchesController < RadioController
+  skip_before_filter :authenticate_user!, :only => :create
+  
+  
+  def create
+    create!{
+      if current_user
+        resource_path(resource)
+      else
+        contact_path
+      end
+    }
+  end
+  
+  protected
+    
+    def authorized?
+      if action_name == "create"
+        true
+      else
+        super()
+      end
+    end
 end
--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,12 +1,6 @@
 class Pitch < ActiveRecord::Base
-  belongs_to :guest, :class_name => "User"
-  belongs_to :contact, :class_name => "User"
   
-  accepts_nested_attributes_for :guest,
-    :reject_if => proc { |attributes| attributes['first_name'].blank? or attributes['email'].blank? }
-      
-  accepts_nested_attributes_for :contact,
-    :reject_if => proc { |attributes| attributes['first_name'].blank? or attributes['email'].blank? }
+  default_scope :order => 'created_at DESC'
     
   def to_s
     title
