9506416de80a79110d28946685bbcc0b2ffef1a3
Taylor Redden <taelor@co-hack.com>
2011-06-20 02:49:07 -0500

    better config for production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,11 @@
+
+exe_path = (RAILS_ENV == "production") ?
+  Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s :
+  '/usr/local/bin/wkhtmltopdf'
+
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => exe_path
 }
+
