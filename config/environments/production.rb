1df87fe222b5bc7eb32ebe8de1c8b4828598db13
Taylor Redden <taelor@co-hack.com>
2014-11-12 10:44:27 -0800

    adding sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,4 +49,14 @@ Radio::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.default_url_options = { :host => 'radio-director.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
